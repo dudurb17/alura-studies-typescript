e259f9a40f28c0edf0f6415cf2f14827439a9d24
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-04 17:29:56 -0300

    separando o scss

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,5 +1,5 @@
 import ITarefa from "../../../types/tarefa";
-import style from "../List.module.scss";
+import style from "./Item.module.scss";
 
 interface Props extends ITarefa {
   selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
@@ -12,13 +12,6 @@ export default function Item({
   id,
   selecionaTarefa,
 }: Props) {
-  console.log("item atual: ", {
-    tarefa,
-    tempo,
-    selecionado,
-    completado,
-    id,
-  });
   return (
     <li
       className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`}
