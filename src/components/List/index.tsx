555a87cd900822039a31b2392e18fd57fcca0c2e
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-04 13:32:23 -0300

    criando a pasta type

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,16 +1,11 @@
 import style from "./List.module.scss";
 import Item from "./Item";
-interface ITarefa {
-  tarefa: string;
-  tempo: string;
-}
+import ITarefa from "../../types/tarefa";
 
 function List({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
-      <h2>
-        Estudos do dia
-      </h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index} {...item} />
