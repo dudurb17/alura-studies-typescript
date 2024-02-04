c618c4233bafff399be5346f459e86ae059f6b51
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-04 19:42:07 -0300

    refatorando o Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{
-  children: string;
+interface Props {
+  children?: React.ReactNode;
   type?: "button" | "submit";
   onClick?: () => void;
-}> {
-  render() {
-    const type = this.props.type || "button";
-    const onClick = this.props.onClick;
-
-    return (
-      <>
-        <button type={type} onClick={onClick} className={style.botao}>
-          {this.props.children}
-        </button>
-      </>
-    );
-  }
 }
+
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button type={type} onClick={onClick} className={style.botao}>
+      {children}
+    </button>
+  );
+}
+
+// class Button1 extends React.Component<{
+//   children: string;
+//   type?: "button" | "submit";
+//   onClick?: () => void;
+// }> {
+//   render() {
+//     const type = this.props.type || "button";
+//     const onClick = this.props.onClick;
+
+//     return (
+//       <>
+//         <button type={type} onClick={onClick} className={style.botao}>
+//           {this.props.children}
+//         </button>
+//       </>
+//     );
+//   }
+// }
 
 export default Button;
