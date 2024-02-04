52dba2f42cc2c0b3efdd00611435db5ee139a3cb
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-04 13:55:07 -0300

    removendo os estaticos

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,19 +3,10 @@ import Form from "../components/Form";
 import List from "../components/List";
 import style from "./App.module.scss";
 import Cronometro from "../components/Cronometro";
+import ITarefa from "../types/tarefa";
 
 function App() {
-  const [tarefas, setTarefas] = useState([
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "JavaScript",
-      tempo: "01:00:03",
-    },
-    { tarefa: "TypeScript", tempo: "03.03.01" },
-  ]);
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
   return (
     <div className={style.AppStyle}>
       <Form setTarefas={setTarefas} />
